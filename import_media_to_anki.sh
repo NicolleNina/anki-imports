1d46619eb65c645ea325ba794317a990e74ef9df
Tyler Rhodes <rhodetyl000@gmail.com>
2021-09-11 18:31:33 -0500

    Add scripts for transfering zip downloads to anki

---
+++ import_media_to_anki.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+input_file=~/anki_downloads
+anki_csv=~/anki_csv
+
+import_media() {
+  for file in $1/*
+    do
+      if [ -d $file ]
+      then
+        import_media $file
+      elif [ -f $file ]
+      then
+        unzip_media $file
+        transfer_to_anki $file
+      fi
+      echo 'Done!'
+    done
+}
+
+
+unzip_media() {
+  if [[ $1 == *.zip ]] 
+  then
+    echo 'unzipping:' $1
+    unzip $file
+  fi
+}
+
+transfer_to_anki() {
+  if [[ $1 != *.zip && $1 != *.csv ]]
+  then
+  echo 'copying media to anki for:' $1
+    cp $1 ~/Library/Application\ Support/Anki2/User\ 1/collection.media/
+  fi
+}
+
+move_csv() {
+  for file in $input_file/*
+    do
+    echo 'current file:' $file
+      if [[ $file == *.csv ]]
+      then
+        echo 'copying:' $file
+        cp $file ~/anki_csv
+      fi
+    done
+}
+
+import_media $input_file
+move_csv
