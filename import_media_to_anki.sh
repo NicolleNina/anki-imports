e5ad86724f53a933d7a7d67c5c8354a8d83824ce
Tyler Rhodes <rhodetyl000@gmail.com>
2022-02-21 15:46:26 -0600

    Remove unnecessary complexity

--- import_media_to_anki.sh
+++ import_media_to_anki.sh
@@ -7,52 +7,47 @@ touch ~/.zshrc
 mkdir -p $INPUT_FILE
 mkdir -p ~/anki_csv
 
-import_media() {
-  local current_dir=$1
+function unzip_media() {
+  for file in $(ls *.zip 2> /dev/null); do
+    echo 'unzipping:' $file
+    unzip $file  -d $INPUT_FILE
+  done
+}
 
-  for file in $current_dir/*;
-  do
-    if [ -d $file ]; then
-      import_media $file
-    elif [ -f $file ]; then
+function import_media() {
+  for file in $(ls ./*/* 2>/dev/null); do
+    if [[ -f $file ]]; then
       transfer_to_anki $file
     fi
   done
-  echo 'Done!'
 }
 
-unzip_media() {
-  for file in $1/*;
-  do
-    if [[ $file == *.zip ]]; then
-      echo 'unzipping:' $file
-      unzip $file  -d $INPUT_FILE
-    fi
-  done
-}
-
-transfer_to_anki() {
+function transfer_to_anki() {
   local transfer_file=$1
-
   if [[ $transfer_file != *.zip && $transfer_file != *.csv ]]; then
     echo "copying media to anki for: $(basename $transfer_file)"
     cp $transfer_file ~/Library/Application\ Support/Anki2/User\ 1/collection.media/
   fi
 }
 
-move_csv() {
-  for file in $INPUT_FILE/*;
-  do
-    echo 'current file:' $file
-    if [[ $file == *.csv ]]; then
-      echo 'copying:' $file
-      cp $file ~/anki_csv
+function move_csv() {
+  for file in $(ls *.csv 2> /dev/null); do
+    if [[ $file ]]; then
+        echo 'copying to anki_csv:' $(basename $file)
+        cp $file ~/anki_csv
     fi
-    echo 'Done!'
   done
-  echo -e "\nGo to Anki and import the file(s) from the anki_csv directory!"
 }
 
-unzip_media $INPUT_FILE
-import_media $INPUT_FILE
+########## Main ###########
+pushd $INPUT_FILE > /dev/null
+
+unzip_media
+import_media
 move_csv
+
+popd > /dev/null
+
+echo 'Done!'
+echo -e "\nGo to Anki and import the file(s) from the anki_csv directory!"
+########## Main ###########
